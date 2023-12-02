afa0f69c760971d91cc4e758c2bbed290f59981a
linuskmr <linuskmr.dev@gmail.com>
2023-12-02 23:22:20 +0100

    Proof of concept

--- src/bin/data_server.rs
+++ src/bin/data_server.rs
@@ -1,7 +1,9 @@
 //! Running on the computer that should be reachable from outside
 
+use std::net::{IpAddr, SocketAddr};
 use std::{error};
 use std::error::Error;
+use anyhow::Context;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 use tokio::{
@@ -13,44 +15,34 @@ use tokio::{
 async fn main() -> Result<(), Box<dyn error::Error>> {
 	env_logger::init();
 
-	// Create connection to proxy server, because proxy server is publicly reachable,
-	// and the router allows *outgoing* connections to the proxy server, but not incoming.
-	let mut proxy = TcpStream::connect("0.0.0.0:5533").await?;
-	// Write port on which the proxy should listen for connections that it will forward to me
-	log::debug!("Write preferred port");
-	proxy.write_all("3344\n".as_bytes()).await?;
+	let proxy_control_addr = "0.0.0.0:5533";
+	let preferred_outside_port: u16 = 3344;
+	let local_server_addr = "127.0.0.1:5000"; // The port of the local server that should be proxied
 
+	let mut proxy = TcpStream::connect(&proxy_control_addr).await.context(format!("Connect to proxy at {}", proxy_control_addr))?;
+	proxy.write_all(format!("{}\n", preferred_outside_port).as_bytes()).await.context(format!("Writing preferred outside port {}", preferred_outside_port))?;
 	let mut proxy = BufReader::new(proxy);
 
 	loop {
-		// Wait for proxy server to announce that a new client wants to connect
 		log::debug!("Waiting for proxy server to announce that a new client wants to connect");
-		let mut target_addr = String::new();
-		proxy.read_line(&mut target_addr).await?;
-		let target_addr = target_addr.strip_suffix('\n').unwrap();
-		log::info!("Connection request to {} forwarded from proxy", target_addr);
+		let mut proxied_client_addr = String::new();
+		proxy.read_line(&mut proxied_client_addr).await.context("Read next proxied client addr from proxy")?;
+		let is_eof = proxied_client_addr.is_empty();
+		if is_eof {
+			log::error!("Proxy server closed connection");
+			break;
+		}
+		let outside_client_addr = proxied_client_addr.strip_suffix('\n').context("Illegal proxied client addr supplied by proxy").unwrap();
+		log::info!("Connection request to {} forwarded from proxy", outside_client_addr);
 
-		log::info!("Creating separate socket to proxy server to handle the new client");
-		let data_source = TcpStream::connect(target_addr).await?;
-		handle_client(data_source).await;
+		tokio::spawn(async move {
+			log::info!("Creating separate socket to proxy server to handle the new client");
+			let mut proxied_client = TcpStream::connect(&proxy_control_addr).await.context("Creating connection to proxy for proxied client")?;
+			let mut local_server = TcpStream::connect(local_server_addr).await.context(format!("Creating connection to local server at {}", local_server_addr))?;
+
+			log::debug!("Copying {:?} - {:?}", proxied_client.peer_addr()?, local_server.peer_addr()?);
+			io::copy_bidirectional(&mut local_server, &mut proxied_client).await.context("Copying between proxied client and local server")
+		});
 	}
-}
-
-async fn handle_client(target: TcpStream) {
-	if let Err(err) = handle_client_inner(target).await {
-		log::error!("{}", err);
-	}
-}
-
-async fn handle_client_inner(mut target: TcpStream) -> Result<(), Box<dyn Error>> {
-	let (mut target_read, mut target_write) = target.split();
-
-
-	let mut outgoing = TcpStream::connect("0.0.0.0:80").await?;
-	let (mut outgoing_read, mut outgoing_write) = outgoing.split();
-	select! {
-		_ = io::copy(&mut target_read, &mut outgoing_write) => (),
-		_ = io::copy(&mut outgoing_read, &mut target_write) => (),
-	};
 	Ok(())
 }
