707807e7cda9427916628cad40c615d557da85e3
linuskmr <linuskmr.dev@gmail.com>
2025-01-27 11:38:09 +0100

    Format code and delete unnecessary files

--- src/bin/origin-proxy.rs
+++ src/bin/origin-proxy.rs
@@ -1,11 +1,14 @@
 //! Running on the computer that should be reachable from outside
 
-use std::{error};
+use std::error;
+
 use anyhow::Context;
 use clap::Parser;
-use tokio::io::{AsyncBufReadExt, BufReader};
-use tokio::net::TcpStream;
-use tokio::io;
+use tokio::{
+	io,
+	io::{AsyncBufReadExt, BufReader},
+	net::TcpStream,
+};
 
 #[derive(clap::Parser, Debug, Clone)]
 struct Cli {
@@ -22,34 +25,47 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
 	env_logger::init();
 	let cli_args = Cli::parse();
 
-	let remote_proxy_control_channel = TcpStream::connect(&cli_args.remote_proxy_addr).await.context(format!("Connecting to proxy at {}", cli_args.remote_proxy_addr))?;
+	let remote_proxy_control_channel = TcpStream::connect(&cli_args.remote_proxy_addr)
+		.await
+		.context(format!("Connecting to proxy at {}", cli_args.remote_proxy_addr))?;
 	let mut remote_proxy_control_channel = BufReader::new(remote_proxy_control_channel);
 
 	loop {
 		log::debug!("Waiting for remote proxy server to announce that a new client wants to connect");
 
 		let mut remote_client_addr = String::new();
-		remote_proxy_control_channel.read_line(&mut remote_client_addr).await.context("Read next proxied client addr from proxy")?;
+		remote_proxy_control_channel
+			.read_line(&mut remote_client_addr)
+			.await
+			.context("Read next proxied client addr from proxy")?;
 		let is_eof = remote_client_addr.is_empty();
 		if is_eof {
 			log::error!("Proxy server closed connection");
 			break;
 		}
 
-		let remote_client_addr = remote_client_addr.strip_suffix('\n').context("Illegal proxied client addr supplied by proxy").unwrap();
+		let remote_client_addr =
+			remote_client_addr.strip_suffix('\n').context("Illegal proxied client addr supplied by proxy").unwrap();
 		log::info!("Connection request from {} forwarded from remote proxy", remote_client_addr);
 
 		tokio::spawn({
 			let cli_args = cli_args.clone();
 			async move {
 				log::info!("Creating socket to remote proxy at {}", &cli_args.remote_proxy_addr);
-				let mut remote_proxy = TcpStream::connect(&cli_args.remote_proxy_addr).await.context(format!("Creating connection to proxy for proxied client at {}", &cli_args.remote_proxy_addr))?;
+				let mut remote_proxy = TcpStream::connect(&cli_args.remote_proxy_addr).await.context(format!(
+					"Creating connection to proxy for proxied client at {}",
+					&cli_args.remote_proxy_addr
+				))?;
 
 				log::debug!("Creating socket to origin server at {}", &cli_args.origin_server_addr);
-				let mut origin_server = TcpStream::connect(&cli_args.origin_server_addr).await.context(format!("Creating connection to local server at {}", &cli_args.origin_server_addr))?;
+				let mut origin_server = TcpStream::connect(&cli_args.origin_server_addr)
+					.await
+					.context(format!("Creating connection to local server at {}", &cli_args.origin_server_addr))?;
 
 				log::debug!("Copying {:?} <-> {:?}", remote_proxy.peer_addr()?, origin_server.peer_addr()?);
-				io::copy_bidirectional(&mut origin_server, &mut remote_proxy).await.context("Copying between proxied client and local server")
+				io::copy_bidirectional(&mut origin_server, &mut remote_proxy)
+					.await
+					.context("Copying between proxied client and local server")
 			}
 		});
 	}
--- src/bin/remote-proxy.rs
+++ src/bin/remote-proxy.rs
@@ -1,13 +1,14 @@
 //! Proxies the connections from clients to the data_server
 
 use std::error::Error;
+
 use anyhow::Context;
 use clap::Parser;
-use tokio::net::TcpListener;
-use tokio::io::{AsyncBufReadExt, BufReader, BufWriter, self};
-use tokio::io::AsyncWriteExt;
-use tokio::sync::mpsc;
-
+use tokio::{
+	io::{self, AsyncWriteExt, BufWriter},
+	net::TcpListener,
+	sync::mpsc,
+};
 
 #[derive(clap::Parser, Debug, Clone)]
 struct Cli {
@@ -17,7 +18,6 @@ struct Cli {
 	#[arg(long)]
 	outside_addr: String,
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -31,8 +31,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let (mut origin_proxy, origin_proxy_addr) = control_socket.accept().await.context("Accepting origin proxy")?;
 	log::info!("Origin proxy connected from {:?}", origin_proxy_addr);
 
-	let (origin_proxy_reader, origin_proxy_writer) = origin_proxy.split();
-	let origin_proxy_reader = BufReader::new(origin_proxy_reader);
+	let (_origin_proxy_reader, origin_proxy_writer) = origin_proxy.split();
 	let mut origin_proxy_writer = BufWriter::new(origin_proxy_writer);
 
 	// Accept connections from the origin proxy and send them to the other thread
@@ -40,28 +39,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	tokio::spawn(async move {
 		loop {
 			let (origin_proxy_client, _) = control_socket.accept().await.context("Accepting inside socket").unwrap();
-			origin_proxy_client_send_channel.send(origin_proxy_client).await.context("Send inside socket to other thread").unwrap();
+			origin_proxy_client_send_channel
+				.send(origin_proxy_client)
+				.await
+				.context("Send inside socket to other thread")
+				.unwrap();
 		}
 	});
 
-	let remote_client = TcpListener::bind(&cli_args.outside_addr).await.context(format!("Listening on {} for remote clients", &cli_args.outside_addr))?;
+	let remote_client = TcpListener::bind(&cli_args.outside_addr)
+		.await
+		.context(format!("Listening on {} for remote clients", &cli_args.outside_addr))?;
 	log::info!("Listening on outside addr: {}", cli_args.outside_addr);
-	
+
 	loop {
 		let (mut remote_client, _) = remote_client.accept().await.context("Accepting remote client")?;
 		log::info!("Remote client connected from {:?}", remote_client.peer_addr()?);
 
 		// Send connection address to server. This indicates that a new client wants to connect.
 		// The origin proxy will thus now create a new separate TCP connection to this program (the remote proxy).
-		origin_proxy_writer.write_all(format!("{:?}\n", remote_client.peer_addr()?).as_bytes()).await.context("Sending outside client addr to server")?;
+		origin_proxy_writer
+			.write_all(format!("{:?}\n", remote_client.peer_addr()?).as_bytes())
+			.await
+			.context("Sending outside client addr to server")?;
 		origin_proxy_writer.flush().await.context("Sending outside client addr to server")?;
-		
-		let mut origin_proxy_client = origin_proxy_client_recv_channel.recv().await.ok_or(anyhow::anyhow!("No origin proxy client available"))?;
+
+		let mut origin_proxy_client =
+			origin_proxy_client_recv_channel.recv().await.ok_or(anyhow::anyhow!("No origin proxy client available"))?;
 		log::info!("Origin proxy client connected from {:?}", origin_proxy_client.peer_addr()?);
-		
+
 		tokio::spawn(async move {
 			log::debug!("Copying {:?} - {:?}", remote_client.peer_addr()?, origin_proxy_client.peer_addr()?);
-			io::copy_bidirectional(&mut origin_proxy_client, &mut remote_client).await.context("Copying between remote client and origin proxy client")
+			io::copy_bidirectional(&mut origin_proxy_client, &mut remote_client)
+				.await
+				.context("Copying between remote client and origin proxy client")
 		});
 	}
 }
